d6adc94e9d43f7d47aa47255943d2eded480c08d
Wpaswan <dablumcapaswan@gmail.com>
2021-12-17 07:59:31 -0800

    [I modified the code to validate email of 8 characters]

--- UserRegistrationProject/Registration.cs
+++ UserRegistrationProject/Registration.cs
@@ -11,6 +11,7 @@ namespace UserRegistrationProject
     {
         public static string FirsName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
         public static string LastName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
+        public static string regex_email = "^[a-z0-9]+(.[a-z0-9]+)?@[a-z]+[.][a-z]{2,3}(.[a-z]{2})?$";
         public bool ValidateFirstName(string uName)
         {
             return Regex.IsMatch(uName, FirsName);
@@ -18,6 +19,10 @@ namespace UserRegistrationProject
         public bool ValidateLasttName(string uName)
         {
             return Regex.IsMatch(uName, LastName);
+        }
+        public bool ValidateEmail(string uName)
+        {
+            return Regex.IsMatch(uName, regex_email);
         }
     }
 }
