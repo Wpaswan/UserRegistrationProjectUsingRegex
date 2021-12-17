d3728b821d975f470a345387bb5f683aa3565f43
Wpaswan <dablumcapaswan@gmail.com>
2021-12-17 07:46:15 -0800

    I added a code to validate First name of user

---
+++ UserRegistrationProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using UserRegistrationProject;
+
+Console.WriteLine("Enter user name:");
+string userName = Console.ReadLine();
+Registration UName = new Registration();
+if (UName.ValidateFirstName(userName)==true)
+    Console.WriteLine("User name accepted");
+else
+    Console.WriteLine("Invalid!!First letter must be capital and minimum 3 characters requiered!!");
---
+++ UserRegistrationProject/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProject
+{
+    internal class Registration
+    {
+        public static string FirsName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
+        public bool ValidateFirstName(string uName)
+        {
+            return Regex.IsMatch(uName, FirsName);
+        }
+    }
+}
